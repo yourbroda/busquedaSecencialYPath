2e0120a6b801d9088c1d422f82c23b38339e3105
yourbroda <yourbroda@gmail.com>
2015-11-12 10:01:54 +0100

    definitivo

---
+++ WindowsApplication8/Class1.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Xml
+Imports System.Xml.XPath
+Public Class Funciones
+    Public Function lecturaSecu(ruta As String) As String 'Lee un archivo xml y añade los nodos a una frase
+        Dim cont As Integer = 1
+        Dim elemento As String
+        Dim frase As String = " "
+        Dim reader As XmlTextReader = New XmlTextReader(ruta)
+        Do While (reader.Read())
+            frase = frase & vbCrLf
+
+            Select Case reader.NodeType
+                Case XmlNodeType.Element
+                    If (reader.Name = elemento) Then
+                        cont = 1
+                    End If
+                    If (cont = 1) Then
+                        elemento = reader.Name
+                    Else
+                        For index As Integer = 1 To cont
+                            frase = frase + ControlChars.Tab
+                        Next
+
+                    End If
+                    cont = cont + 1
+
+                    frase = frase & "<" & reader.Name
+                    If (reader.HasAttributes) Then
+                        While reader.MoveToNextAttribute
+                            frase = frase + reader.Name & ">" & reader.Value
+                        End While
+                    End If
+                    frase = frase & ">"
+
+                Case XmlNodeType.Text
+                    If (reader.Name = elemento) Then
+                        cont = 2
+                    End If
+                    If (cont = 2) Then
+                        elemento = reader.Name
+                    Else
+                        For index As Integer = 1 To cont
+                            frase = frase & ControlChars.Tab
+                        Next
+
+                    End If
+                    frase = frase & reader.Value
+
+                Case XmlNodeType.EndElement
+                    cont = cont - 1
+                    If (reader.Name = elemento) Then
+                        cont = 1
+                    End If
+                    If (cont = 1) Then
+                        elemento = reader.Name
+                    Else
+                        For index As Integer = 1 To cont
+                            frase = frase & ControlChars.Tab
+                        Next
+
+                    End If
+                    frase = frase & "</" + reader.Name
+                    frase = frase & ">"
+                    frase = frase & vbCrLf
+
+
+
+            End Select
+
+        Loop
+
+
+        Return frase
+    End Function
+    Public Function Consulta(ruta As String, path As String) As String 'Busca todo los nodos que cuadren con el path y los devuelve en una frase
+        Dim frase As String = ""
+
+        Dim docNav As XPathDocument
+
+        Dim nav As XPathNavigator
+
+        Dim nodeIter As XPathNodeIterator
+
+        Dim strExpression As String
+
+
+        docNav = New XPathDocument(ruta)
+
+
+        nav = docNav.CreateNavigator
+
+
+        strExpression = path
+
+        nodeIter = nav.Select(strExpression)
+
+        If nodeIter.Count() > 0 Then
+
+
+            While (nodeIter.MoveNext())
+                frase = frase & nodeIter.Current.Name & ": "
+                frase = frase & nodeIter.Current.Value.ToString & vbCrLf
+                frase = StrConv(frase, vbProperCase)
+            End While
+        Else
+            frase = frase & "Error xpath"
+        End If
+        Return frase
+    End Function
+End Class
---
+++ WindowsApplication8/Form1.vb
@@ -0,0 +1,37 @@
+﻿
+Public Class Form1
+    Dim funcion As New Funciones
+    Dim direccion As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Vista XML"
+        Label2.Text = "Consulta"
+        Button2.Text = "Buscar XML"
+        Button1.Text = "Realizar Consulta"
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        RichTextBox2.Text = funcion.Consulta(direccion, TextBox1.Text)
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        direccion = Ruta()
+        RichTextBox1.Text = funcion.lecturaSecu(direccion)
+
+    End Sub
+    Private Function Ruta() As String 'Funcion de busqueda de archivo
+        Dim file As New OpenFileDialog
+        Dim rut As String = " "
+        file.Filter = "Tipo de arcivo *|*"
+        file.FileName = "Archivo de programa"
+        If (file.ShowDialog = 1) Then
+            rut = file.FileName
+        End If
+        Return rut
+    End Function
+End Class
